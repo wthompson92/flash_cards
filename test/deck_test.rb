421e606547d512bd5864a5bb9c08258b511fb9c4
username <wnt925@gmail.com>
2019-05-01 22:13:53 -0600

    Update deck test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,19 +1,17 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/card'
-require '../lib/turn'
-require '../lib/deck'
-
+require './lib/deck'
+require './lib/round'
 class DeckTest < Minitest::Test
   attr_reader :card_1, :card_2, :card_3, :cards, :deck
 
-def setup
-  @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  @cards = [card_1, card_2, card_3]
-  @deck = Deck.new(@cards)
-end
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @cards = [card_1, card_2, card_3]
+    @deck = Deck.new(@cards)
+  end
 
   def test_it_exists
     actual = Deck
@@ -32,12 +30,15 @@ end
   def test_it_counts
     expected = @deck.count
     actual = 3
+
     assert_equal expected, actual
   end
 
   def test_it_seperates_by_category
     expected = @deck.cards_in_category(:STEM)
     actual = [@card_2, @card_3]
-assert_equal expected, actual
+
+    assert_equal expected, actual
   end
+
 end
