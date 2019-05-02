524e9167a866b0b7c9da4566594aa594b8b4c8fa
username <wnt925@gmail.com>
2019-05-01 22:12:54 -0600

    Add round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,3 @@
-require '../lib/turn'
-require '../lib/card'
-require '../lib/deck'
 
 class Round
   attr_reader :deck,
@@ -8,24 +5,46 @@ class Round
               :current_card,
               :number_correct
 
+
   def initialize(deck)
     @deck = deck
     @turns = []
     @current_card = @deck.cards[0]
     @number_correct = 0
+
   end
 
   def take_turn(guess)
     new_turn = Turn.new(guess, @current_card)
     @turns << new_turn
-      if guess == card.answer
-        @number_correct += 1
-      end
+  
     new_turn
+
   end
 
+  def number_correct
+    @guess == @current_card.answer
+        @number_correct += 1
+      end
+
+
   def count
-    @turn.count
+    @turn.length
   end
-  
-end
+
+  def number_correct_by_category(category)
+   @turns.count do |turn| turn.correct? && category == @current_card.category
+    end
+  end
+
+
+  def percent_correct
+  number_correct.to_f / @turns.length * 100
+  end
+
+  def percent_correct_by_category(category)
+     num_in_cat = turns.count { |turn| turn.card.category == category}
+     100 * number_correct_by_category(category).to_f / num_in_cat
+   end
+
+  end
