c7e9917b2a4761577cd88a59e1f78a7e3f143cb3
username <wnt925@gmail.com>
2019-05-05 23:41:38 -0600

    Update deck test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,15 +1,16 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
 require './lib/deck'
-require './lib/round'
+
 class DeckTest < Minitest::Test
-  attr_reader :card_1, :card_2, :card_3, :cards, :deck
 
   def setup
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    @cards = [card_1, card_2, card_3]
+    @cards = [@card_1, @card_2, @card_3]
     @deck = Deck.new(@cards)
   end
 
@@ -20,25 +21,31 @@ class DeckTest < Minitest::Test
     assert_instance_of actual, expected
   end
 
-  def test_it_display_cards
-      expected = @deck.cards
-      actual = @cards
+  def test_it_has_cards
+      expected = @cards
+      actual = @deck.cards
 
-    assert_equal @deck.cards, @cards
+    assert_equal expected, actual
   end
 
-  def test_it_counts
-    expected = @deck.count
-    actual = 3
+  def test_it_counts_cards_in_deck
+    expected = 3
+    actual =  @deck.count
 
     assert_equal expected, actual
   end
 
   def test_it_seperates_by_category
-    expected = @deck.cards_in_category(:STEM)
-    actual = [@card_2, @card_3]
-
+      expected = [@card_2, @card_3]
+      actual = @deck.cards_in_category(:STEM)
     assert_equal expected, actual
-  end
 
-end
+      expected = [@card_1]
+      actual = @deck.cards_in_category(:Geography)
+    assert_equal expected, actual
+
+      expected = []
+      actual =  @deck.cards_in_category("Pop Culture")
+    assert_equal expected, actual
+    end
+  end
