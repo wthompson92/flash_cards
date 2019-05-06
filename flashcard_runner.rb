1642bab7ca9e1fb6d704e90e94f81fd26b003161
username <wnt925@gmail.com>
2019-05-06 00:33:36 -0600

    Update flash card runner

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,12 +2,18 @@ require './lib/round'
 require './lib/deck'
 require './lib/turn'
 require './lib/card'
+require './lib/card_generator'
 
-card_1 = Card.new("What is 5 + 5?", '10', :math)
-card_2 = Card.new("What is Rachel's favorite animal?", "panda", :turing_staff)
-card_3 = Card.new("What is Mike's middle name?", "nobody knows", :turing_staff)
-card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :pop_culture)
-cards = [card_1, card_2, card_3, card_4]
-deck = Deck.new(cards)
-round = Round.new(deck)
+filename = 'cards.txt'
+cards = CardGenerator.new(filename).cards
+deck     = Deck.new(cards)
+round    = Round.new(deck)
+
+#card_1 = Card.new("What is 5 + 5?", '10', :math)
+#card_2 = Card.new("What is Rachel's favorite animal?", "panda", :turing_staff)
+#card_3 = Card.new("What is Mike's middle name?", "nobody knows", :turing_staff)
+#card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :pop_culture)
+#cards = [card_1, card_2, card_3, card_4]
+#deck = Deck.new(cards)
+#round = Round.new(deck)
 round.start
