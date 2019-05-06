92732badb4b95d90f13fb6f4f6a036bea9e5427e
username <wnt925@gmail.com>
2019-05-05 23:45:32 -0600

    Update turn test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,45 +1,60 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/card'
-require '../lib/turn'
+require './lib/card'
+require './lib/turn'
 
 class TurnTest < Minitest::Test
-  attr_reader :card,
-              :turn
 
-def setup
+  def setup
     @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    @turn = Turn.new("Juneau", card)
+    @turn = Turn.new("Juneau", @card)
+    @card_2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    @turn_2 = Turn.new("Saturn", @card_2)
   end
 
   def test_it_exists
     expected = Turn
-    actual = turn
-    assert_instance_of Turn, turn
+    actual = @turn
+    assert_instance_of Turn, @turn
   end
 
-  def test_card
-    expected = card
-    actual = turn.card
+  def test_turn_has_a_card
+      expected = @card
+      actual = @turn.card
+    assert_equal expected, actual
 
+      expected = @card_2
+      actual = @turn_2.card
     assert_equal expected, actual
   end
 
-  def test_guess
+  def test_it_has_a_guess
       expected = "Juneau"
-      actual = turn.guess
+      actual = @turn.guess
+    assert_equal expected, actual
 
+      expected = "Saturn"
+      actual = @turn_2.guess
     assert_equal expected, actual
   end
 
-  def test_correct?
-    actual = turn.correct?
+  def test_guess_can_correct
+      actual = @turn.correct?
     assert actual
   end
 
-def test_feedback
-  expected = "Correct"
-  actual = turn.feedback
+  def test_guess_can_incorrect
+      actual = @turn_2.correct?
+    refute actual
+  end
+
+  def test_feedback
+    expected = "Correct!"
+    actual = @turn.feedback
   assert_equal expected, actual
-end
+
+    expected = "Incorrect!"
+    actual = @turn_2.feedback
+  assert_equal expected, actual
+  end
 end
